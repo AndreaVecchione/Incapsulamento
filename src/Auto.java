ca877d782c1171feee1317b2e8c4bf966e6345e1
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-05-19 20:24:28 +0200

    Update Auto.java

--- src/Auto.java
+++ src/Auto.java
@@ -1,49 +1,51 @@
-public class Auto {
-    private int cilindrata;
-    private int modello;
-    private String marca;
-    private String targa;
-    public Auto (int cilindrata, int modello, String marca, String targa) {
+public class ValoriAuto {
+// Creazione delle variabili dell'auto
+        private String modello;
+        private String cilindrata;
+        private String targa;
+        private String marca;
+        public ValoriAuto (String modello, String cilindrata, String targa, String marca) {
+// Utilizzo della parola chiave this per la lettura dei valori presenti nel main di automobile.
+            this.modello = modello;
+            this.cilindrata = cilindrata;
+            this.targa = targa;
+            this.marca = marca;
 
-        this.cilindrata = cilindrata;
-        this.modello = modello;
-        this.marca = marca;
-        this.targa = targa;
-
-    }
-    public int getCilindrata() {
-        return cilindrata;
-    }
-public int getModello() {
-        return modello;
-}
-public String getMarca() {
-        return marca;
-}
-public String getTarga() {
-        return targa;
-}
-public void setCilindrata(int cilindrata) {
-        this.cilindrata = cilindrata;
-}
-public void setModello (int modello) {
-        this.modello = modello;
-    }
-    public void setMarca (String marca) {
-        this.marca = marca;
-    }
-    public void setTarga (String targa) {
-        this.targa = targa;
-    }
-@Override
-    public String toString() {
-    return "Auto: {" +
-            "Cilindrata = " + cilindrata +
-            ", Modello = " + modello  +
-            ", Marca = " + marca   +
-            ", Targa = " + targa +
-            '}';
-}
+        }
+        // Uso get and set per usare le variabili private nel Main.
+        public String getModello() {
+            return modello;
+        }
+        public String getCilindrata() {
+            return cilindrata;
+        }
+        public String getTarga() {
+            return targa;
+        }
+        public String getMarca() {
+            return marca;
+        }
+        public void setModello(String modello) {
+            this.modello = modello;
+        }
+        public void setCilindrata (String cilindrata) {
+            this.cilindrata = cilindrata;
+        }
+        public void setTarga (String targa) {
+            this.targa = targa;
+        }
+        public void setmarca (String marca) {
+            this.marca = marca;
+        }
+        @Override
+        public String toString() {
+            return "Auto: {" +
+                    "Modello = " + modello +
+                    ", Cilindrata = " + cilindrata  +
+                    ", Targa = " + targa   +
+                    ", Marca = " + marca +
+                    '}';
+        }
 }
 
 
